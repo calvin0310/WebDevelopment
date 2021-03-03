377f15e4aac1a59b5712fc33154259f8cda46eb9
Calvin Wang <s3101990@hotmail.com>
2021-03-03 13:33:26 +0800

    20210303

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -24,6 +25,23 @@ app.get("/", function (req, res) {
     res.render("home", {
         startingContent: homeStartingContent,
         posts: posts
+    });
+});
+
+app.get("/posts/:postName", function(req, res) {
+    const requestedTitle = _.lowerCase(req.params.postName);
+
+    posts.forEach(function(post) {
+        const storedTitle = _.lowerCase(post.title);
+
+        if (storedTitle === requestedTitle) {
+            res.render("post", {
+                post: post
+            })
+            
+        } else {
+            console.log("Not a match!");
+        }
     });
 });
 
@@ -49,7 +67,6 @@ app.post("/compose", function (req, res) {
         content: req.body.postBody
     }; 
     posts.push(post);
-    console.log(posts);
     res.redirect("/");
 });
 
