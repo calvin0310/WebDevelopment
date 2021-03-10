13603602ddfb3c02e8f174221da56297ea387bf8
Calvin Wang <s3101990@hotmail.com>
2021-03-10 13:42:45 +0800

    20210310

--- FruitsProject/app.js
+++ FruitsProject/app.js
@@ -1,52 +1,71 @@
 // jshint esversion:6
 
-const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const mongoose = require('mongoose');
 
-// Connection URL
-const url = 'mongodb://localhost:27017';
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// Database Name
-const dbName  = 'fruitsDB';
-
-// Create a new MongoClient
-const client = new MongoClient(url, { useUnifiedTopology: true });
-
-// Use connect method to connect to the Server
-client.connect(function(err) {
-    assert.strictEqual(null, err);
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    client.close();
+const fruitSchema = new mongoose.Schema ({
+    name: String,
+    score: Number,
+    review: String
 });
 
-const insertDocuments = function(db, callback) {
+const Fruit = mongoose.model("Fruit", fruitSchema);
+
+const kiwi = new Fruit ({
+    name: "Kiwi",
+    score: 10,
+    review: "The best fruit!"
+});
+
+const orange = new Fruit ({
+    name: "Orange",
+    score: 4,
+    review: "Too sour for me"
+});
+
+const banana = new Fruit ({
+    name: "Banana",
+    score: 3,
+    review: "Weird texture"
+});
+
+Fruit.insertMany([kiwi, orange, banana], function(err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully saved all the fruits to fruitsDB");
+    }
+});
+// const fruit = new Fruit ({
+//     name: "Apple",
+//     rating: 7,
+//     review: "Pretty solid as fruit."
+// });
+
+const personSchema = new mongoose.Schema ({
+    name: String,
+    age: Number
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person ({
+    name: "John",
+    age: 37
+});
+
+// fruit.save();
+person.save();
+
+const findDocuments = function(db, callback) {
     // Get the documents collection
     const collection = db.collection('fruits');
-    // Insert some documents
-    collection.insertMany([
-        {
-            name: "Apple",
-            score: 8,
-            review: "Great fruit"
-        }, 
-        {
-            name: "Orange",
-            score: 6,
-            review: "Kinda sour"
-        },
-        {
-            name: "Banana",
-            score: 9,
-            review: "Great stuff!"
-        }
-    ], function(err, result) {
+    // Find same documents
+    collection.find({}).toArray(function(err, fruits) {
         assert.strictEqual(err, null);
-        assert.strictEqual(3, result.result.n);
-        assert.strictEqual(3, result.ops.length);
-        console.log("Inserted 3 documents into the collection");
-        callback(result);
+        console.log("Found the following records");
+        console.log(fruits);
+        callback(fruits);
     });
-}
+};
---
+++ FruitsProject/app_mongodb.js
@@ -0,0 +1,66 @@
+// jshint esversion:6
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName  = 'fruitsDB';
+
+// Create a new MongoClient
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+    assert.strictEqual(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    findDocuments(db, function() {
+        client.close();
+    });  
+});
+
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Insert some documents
+    collection.insertMany([
+        {
+            name: "Apple",
+            score: 8,
+            review: "Great fruit"
+        }, 
+        {
+            name: "Orange",
+            score: 6,
+            review: "Kinda sour"
+        },
+        {
+            name: "Banana",
+            score: 9,
+            review: "Great stuff!"
+        }
+    ], function(err, result) {
+        assert.strictEqual(err, null);
+        assert.strictEqual(3, result.result.n);
+        assert.strictEqual(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    });
+};
+
+const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('fruits');
+    // Find same documents
+    collection.find({}).toArray(function(err, fruits) {
+        assert.strictEqual(err, null);
+        console.log("Found the following records");
+        console.log(fruits);
+        callback(fruits);
+    });
+};
