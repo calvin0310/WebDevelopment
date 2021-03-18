d9ed5c1dc602e230b88e821f3247c2f764f6ccb6
Calvin Wang <s3101990@hotmail.com>
2021-03-18 14:06:32 +0800

    20210318

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
+const _ = require("lodash");
 
 const app = express();
 
@@ -67,7 +67,7 @@ app.get("/", function (req, res) {
 });
 
 app.get("/:customListName", function (req, res) {
-    const customListName = req.params.customListName;
+    const customListName = _.capitalize(req.params.customListName);
 
     List.findOne(
         {
@@ -94,28 +94,64 @@ app.get("/:customListName", function (req, res) {
             }
         }
     );
-
 });
 
 app.post("/", function (req, res) {
     const itemName = req.body.newItem;
+    const listName = req.body.list;
 
     const item = new Item({
         name: itemName,
     });
 
-    item.save();
-    res.redirect("/");
+    if (listName === "Today") {
+        item.save();
+        res.redirect("/");
+    } else {
+        List.findOne(
+            {
+                name: listName,
+            },
+            function (err, foundList) {
+                foundList.items.push(item);
+                foundList.save();
+                res.redirect("/" + listName);
+            }
+        );
+    }
 });
 
 app.post("/delete", function (req, res) {
     const checkedItemId = req.body.checkbox;
-    Item.findByIdAndRemove(checkedItemId, function (err) {
-        if (!err) {
-            console.log("Successfully deleted checked item.");
-            res.redirect("/");
-        }
-    });
+
+    const listName = req.body.listName;
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId, function (err) {
+            if (!err) {
+                console.log("Successfully deleted checked item.");
+                res.redirect("/");
+            }
+        });
+    } else {
+        List.findOneAndUpdate(
+            {
+                name: listName,
+            },
+            {
+                $pull: {
+                    items: {
+                        _id: checkedItemId,
+                    },
+                },
+            },
+            function (err, foundList) {
+                if (!err) {
+                    res.redirect("/" + listName);
+                }
+            }
+        );
+    }
 });
 
 app.get("/about", function (req, res) {
